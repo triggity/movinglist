a4d637daab27999e8afc444a302339d0d99166a4
Michael Truong <mtruong@classpass.com>
2017-12-27 23:45:31 -0800

    filterable table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -4,7 +4,12 @@ import * as ReactTable from 'react-table';
 const Table = (props) => (
     <ReactTable.default
         data={props.data}
+        filterable={true}
+        defaultFilterMethod={(filter, row) =>
+          String(row[filter.id]).toLowerCase().includes(filter.value)}
         columns={props.columns}
+        defaultPageSize={100}
+        className="-striped -highlight"
     />
 )
 export default Table;
